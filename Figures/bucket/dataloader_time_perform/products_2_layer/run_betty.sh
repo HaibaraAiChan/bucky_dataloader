4edd77094762823ac9330c7243249d2a7310f135
Chameleon Cloud User <cc@ysy-rtx-6000-4.novalocal>
2024-04-16 21:58:41 +0000

    ogbn-products betty random metis log hidden 128 done

--- Figures/bucket/dataloader_time_perform/products_2_layer/run_betty.sh
+++ Figures/bucket/dataloader_time_perform/products_2_layer/run_betty.sh
@@ -6,7 +6,8 @@ do
 
     save_path=./betty_log/${md}
     # for nb in 16 24 32 17 18 19 20 21 22
-    for nb in  7 8 9 10 11 12 13 14 15 16 24 32 17 18 19 20
+    # for nb in  7 8 9 10 11 12 13 14 15 16 24 32 17 18 19 20
+    for nb in  17 18 19 20
     do
         echo "---start  hidden ${hidden},  nb ${nb} batches"
         python Betty_e2e_time.py  \
@@ -25,46 +26,3 @@ do
     done
 done
 
-save_path=./range_random_metis/SAGE/h_128
-mkdir $save_path
-
-hidden=128
-folder=range
-for nb in 14 15 16 17 18 19 20 22 24 32
-do
-    echo "---start  $nb batches"
-    python bucky_time_2.py \
-        --dataset ogbn-products \
-        --selection-method range_bucketing \
-        --num-batch $nb \
-        --mem-constraint 18.1 \
-        --num-layers 2 \
-        --fan-out 10,25 \
-        --num-hidden $hidden \
-        --num-runs 1 \
-        --num-epoch 20 \
-        --aggre lstm \
-        --log-indent 3 \
-        --lr 1e-2 \
-    > "${save_path}/${folder}/nb_${nb}.log"
-done
-
-folder=random
-for nb in  17 18 19 20 22 
-do
-    echo "---start  $nb batches"
-    python bucky_time_2.py \
-        --dataset ogbn-products \
-        --selection-method random_bucketing \
-        --num-batch $nb \
-        --mem-constraint 18.1 \
-        --num-layers 2 \
-        --fan-out 10,25 \
-        --num-hidden $hidden \
-        --num-runs 1 \
-        --num-epoch 20 \
-        --aggre lstm \
-        --log-indent 3 \
-        --lr 1e-2 \
-    > "${save_path}/${folder}/nb_${nb}.log"
-done
